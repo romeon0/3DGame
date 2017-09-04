b7f68416708669f4d62d8b46872ee6b7a651c6a0
Romeon0 <Romeon0@mail.ru>
2017-09-04 19:22:24 +0300

    Commited all /build folder.

---
+++ x64/Debug/fsColorShader.glsl
@@ -0,0 +1,7 @@
+#version 330 core
+in vec3 color;
+out vec3 outColor;
+
+void main(){
+	outColor = color;
+}
---
+++ x64/Debug/fsTextureShader.glsl
@@ -0,0 +1,23 @@
+#version 330 core
+out vec4 FragColor;
+
+in vec2 TexCoords;
+in vec3 pos;
+
+uniform sampler2D texture_diffuse1;
+
+void main()
+{    
+	//if(pos.x>=1.0 && pos.x<=1.1)
+	float xMod = mod(pos.x,2.0);
+	float yMod = mod(pos.y,2.0);
+	float zMod = mod(pos.z,2.0);
+	/*if(xMod>=0.0 && xMod<=0.1)
+		FragColor=vec4(0.3,0.1,0.8,1.0);
+	//else if(yMod>=0.0 && yMod<=0.1)
+	//	FragColor=vec4(0.3,0.1,0.8,1.0);
+	else if(zMod>=0.0 && zMod<=0.1)
+		FragColor=vec4(0.3,0.1,0.8,1.0);
+	else*/
+		FragColor = texture(texture_diffuse1, TexCoords);
+}
